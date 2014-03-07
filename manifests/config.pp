1997db16b099498aa815eb78dad53332bc44f641
Andreas Schröder <kaazoo@artcom.de>
2014-03-07 12:31:12 +0100

    modify nginx defaults add certificate files fix indention & modify certificate paths

--- manifests/config.pp
+++ manifests/config.pp
@@ -16,8 +16,50 @@ class talk-production::config {
   
   # Nginx configuration
   file { '/etc/nginx/nginx.conf':
-    ensure => present,
-    source => 'puppet:///modules/talk-production/nginx/nginx.conf',
+    ensure  => present,
+    source  => 'puppet:///modules/talk-production/nginx/nginx.conf',
     require => Exec['/root/nginx-install/install.sh'],
+    notify  => Service['nginx'],
   }
+
+  augeas{ "nginx-defaults" :
+    context => "/etc/default/nginx",
+    changes => "set ULIMIT '-n 202000'",
+    onlyif  => "match other_value size > 0",
+    require => Exec['/root/nginx-install/install.sh'],
+    notify  => Service['nginx'],
+  }
+
+  file { '/etc/ssl/certs/filecache-cert.pem':
+    ensure  => present,
+    source  => 'puppet:///modules/talk-production/ssl/filecache-cert.pem',
+    require => Exec['/root/nginx-install/install.sh'],
+    notify  => Service['nginx'],
+  }
+
+  file { '/etc/ssl/private/filecache-key.pem':
+    ensure  => present,
+    source  => 'puppet:///modules/talk-production/ssl/filecache-key.pem',
+    require => Exec['/root/nginx-install/install.sh'],
+    notify  => Service['nginx'],
+  }
+
+  file { '/etc/ssl/certs/server-cert.pem':
+    ensure  => present,
+    source  => 'puppet:///modules/talk-production/ssl/server-cert.pem',
+    require => Exec['/root/nginx-install/install.sh'],
+    notify  => Service['nginx'],
+  }
+
+  file { '/etc/ssl/private/server-key.pem':
+    ensure  => present,
+    source  => 'puppet:///modules/talk-production/ssl/server-key.pem',
+    require => Exec['/root/nginx-install/install.sh'],
+    notify  => Service['nginx'],
+  }
+
+  service { 'nginx':
+    ensure => running,
+  }
+
 }
