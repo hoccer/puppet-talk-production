3b17a723f42102c05d0b61802bf508abef921ff5
Andreas Schröder <kaazoo@artcom.de>
2014-03-11 14:03:06 +0100

    send email when restarting service

--- files/filecache/restart_filecache.sh
+++ files/filecache/restart_filecache.sh
@@ -6,10 +6,11 @@ service filecache status | grep start >/dev/null
 if [ "$?" -eq "0" ]; then
   echo "filecache upstart job is running."
   curl -k -s -f -m 5 https://filecache1.talk.hoccer.de/status >/dev/null
-
-  if [ "$?" -ne "0" ]; then
+  ret=$?
+  if [ "$ret" -ne "0" ]; then
     echo "filecache service seems to hang. Restarting."
     service filecache restart
+    echo "Filecache service has been restarted. Return value of curl was $ret. See curl manpage." | mail -s "Hoccer: Filecache service has been restarted." admins@hoccer.com
   else
     echo "filecache service seems to run fine."
   fi
