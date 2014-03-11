630f221b89b8281cd906f142c843786ba1567112
Andreas Schröder <kaazoo@artcom.de>
2014-03-11 16:35:42 +0100

    use full paths use status & restart tools

--- files/filecache/restart_filecache.sh
+++ files/filecache/restart_filecache.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # check if upstart job was started
-service filecache status | grep start >/dev/null
+/sbin/status filecache | /bin/grep running >/dev/null
 
 if [ "$?" -eq "0" ]; then
   echo "filecache upstart job is running."
-  curl -k -s -f -m 5 https://filecache1.talk.hoccer.de/status >/dev/null
+  /usr/bin/curl -k -s -f -m 5 https://filecache1.talk.hoccer.de/status >/dev/null
   ret=$?
   if [ "$ret" -ne "0" ]; then
     echo "filecache service seems to hang. Restarting."
-    service filecache restart
+    /sbin/restart filecache
     echo "Filecache service has been restarted. Return value of curl was $ret. See curl manpage." | mail -s "Hoccer: Filecache service has been restarted." admins@hoccer.com
   else
     echo "filecache service seems to run fine."
