3ddd86d528c1ff10979515ee7238262eb3f88df4
Andreas Schröder <kaazoo@artcom.de>
2014-05-20 11:41:54 +0200

    add missing variables to class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,6 @@
-class talk-production ($talkserver_fqdn, $talkserver_cert, $talkserver_key,
-                       $filecache_fqdn, $filecache_cert, $filecache_key) {
+class talk-production ($talkserver_fqdn, $talkserver_port, $talkserver_cert, $talkserver_key,
+		$legacy_talkserver_fqdn, $legacy_talkserver_port, $legacy_talkserver_cert, $legacy_talkserver_key,
+		$filecache_fqdn, $filecache_port, $filecache_cert, $filecache_key,
+		$legacy_filecache_fqdn, $legacy_filecache_port, $legacy_filecache_cert, $legacy_filecache_key) {
   include talk-production::install, talk-production::config
 }
