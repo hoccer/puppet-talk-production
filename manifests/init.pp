0483fe008c6575b9974bd9fb7d37224f0681c645
Guido Lorenz <guido.lorenz@artcom.de>
2014-09-26 10:23:37 +0200

    Fix whitespace

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,6 @@ class talk-production ($primary_talkserver_fqdn, $primary_talkserver_port,
         $primary_talkserver_backend, $secondary_talkserver_fqdn,
         $secondary_talkserver_port, $secondary_talkserver_cert,
         $secondary_talkserver_key, $secondary_talkserver_backend,
-		$filecache_fqdn, $filecache_port, $filecache_cert, $filecache_key) {
+        $filecache_fqdn, $filecache_port, $filecache_cert, $filecache_key) {
   include talk-production::install, talk-production::config
 }
