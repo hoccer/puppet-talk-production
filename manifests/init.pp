570cd371863c695c246eb2aea1ebd7e8cd0ea2ba
Guido Lorenz <guido.lorenz@artcom.de>
2014-09-26 10:49:26 +0200

    Add invitation server config to talk-production class

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,7 @@ class talk-production ($primary_talkserver_fqdn, $primary_talkserver_port,
         $primary_talkserver_backend, $secondary_talkserver_fqdn,
         $secondary_talkserver_port, $secondary_talkserver_cert,
         $secondary_talkserver_key, $secondary_talkserver_backend,
-        $filecache_fqdn, $filecache_port, $filecache_cert, $filecache_key) {
+        $filecache_fqdn, $filecache_port, $filecache_cert, $filecache_key,
+        $invitation_server_fqdn, $invitation_server_port, $invitation_uri_scheme) {
   include talk-production::install, talk-production::config
 }
